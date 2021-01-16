1605d50f5e939cc565fb76012404c3a11510291a
wulan17 <galihgustip@gmail.com>
2021-01-16 21:51:11 +0700

    add support for armeabi-v7a devices with Android 10
    Signed-off-by: wulan17 <galihgustip@gmail.com>

--- README.md
+++ README.md
@@ -64,7 +64,9 @@ All of the above commits are also available in the form of patch files [in this 
 The keystore executables and libraries in this repository were built with the commits linked above. The target CPU was changed to generic ARMv8-A for all target devices.
 
 - Android 11: Built from ProtonAOSP 11.3.1 (android-11.0.0_r24) for `redfin`
-- Android 10: Built from LineageOS 17.1 (android-10.0.0_r41) for `taimen`
+- Android 10:  
+  arm64 : Built from LineageOS 17.1 (android-10.0.0_r41) for `taimen`  
+  arm32 : Built from Nusantara-Rom (android-10.0.0_r41) for `cactus`
 - Android 9: Built from AOSP android-9.0.0_r61 for `taimen`
 - Android 8.1: Built from AOSP android-8.1.0_r81 for `taimen`
 - Android 8.0: Built from AOSP android-8.0.0_r51 for `marlin`
--- customize.sh
+++ customize.sh
@@ -4,18 +4,22 @@
 # service, and underlying AIDL/HIDL ABIs may not match. We also link against other
 # system libraries.
 arch="$(getprop ro.product.cpu.abi)"
-if [[ "$arch" != "arm64-v8a" ]]; then
-    ui_print "Unsupported CPU architecture: $arch"
+if [[ "$arch" == "arm64-v8a" ]]; then
+    abi="64";
+elif [[ "$arch" == "armeabi-v7a" ]];then
+	abi="32";
+else
+	ui_print "Unsupported CPU architecture: $arch"
     exit 1
 fi
 
 sdk="$(getprop ro.build.version.sdk)"
 version="$(getprop ro.vendor.build.version.release)"
 
-if mv "$MODPATH/system_sdk$sdk" $MODPATH/system; then
-    ui_print "Installing for Android $version"
+if mv "$MODPATH/system_sdk""$sdk""_""$abi" $MODPATH/system; then
+    ui_print "Installing for Android $version $arch"
 else
-    ui_print "Android $version (SDK $sdk) is not supported!"
+    ui_print "Android $version $arch (SDK $sdk) is not supported!"
     exit 1
 fi
 
